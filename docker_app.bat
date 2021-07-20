12454be11d3940a8b677f328d845c2ce20f6e26a
RK <rkehrli1993@gmail.com>
2021-07-20 03:13:03 -0400

    added name to docker container

--- docker_app.bat
+++ docker_app.bat
@@ -1,3 +1,3 @@
 :: This Will build an image of the app and server it on localhost:4000
 docker build -t "express:app" ./
-docker run --rm -d -p 4000:4000 express:app
+docker run --rm -d -p 4000:4000 --name typescript_backend express:app
